d4b49cde440d4eea3de42e7d07ce8901a0f80238
rafallyczek <56969152+rafallyczek@users.noreply.github.com>
2020-03-01 13:20:30 +0100

    Add files via upload

---
+++ src/test/java/njpo2/project/bookrepo/BookrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package njpo2.project.bookrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
